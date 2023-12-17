01fdd79b8ddf56dfd28bae08521b1e376e48b8b5
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-12-17 22:00:25 +0530

    progress bar

--- ProgressSteps/src/App.tsx
+++ ProgressSteps/src/App.tsx
@@ -2,18 +2,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [completedSteps, setCompletedSteps] = useState({})
+  const [activeSteps, setActiveSteps] = useState(1);
+  const [totalCircles, setTotalCircles] = useState(4);
+  console.log("activeSteps... ", activeSteps)
+  console.log("totalCircles... ", totalCircles)
+
+  console.log(((activeSteps - 1) / (totalCircles - 1)) * 100 + "%");
+
+  const nextHandler = () => {
+    setActiveSteps(activeSteps + 1)
+  }
+
+  const prevHandler = () => {
+    setActiveSteps(activeSteps - 1)
+  }
   return (
     <div className="parent-container">
       <div className="child-container">
+        <div
+          className="progress"
+          id="progress"
+          style={{
+            width: `${((activeSteps - 1) / (totalCircles - 1)) * 100 + "%"} `,
+          }}
+        ></div>
         <div className="circle active">1</div>
-        <div className="circle">2</div>
+        <div className="circle active">2</div>
         <div className="circle">3</div>
         <div className="circle">4</div>
       </div>
       <div className="btn-container">
-        <button className="prev">Prev</button>
-        <button className="next">Next</button>
+        <button className="prev" onClick={prevHandler}>Prev</button>
+        <button className="next" onClick={nextHandler}>Next</button>
       </div>
     </div>
   );
