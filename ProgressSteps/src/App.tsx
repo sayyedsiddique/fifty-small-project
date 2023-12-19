4ee7964b132af70083ac5eec27761b40ce0d01e2
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-12-19 22:22:05 +0530

    progress steps project is completed now

--- ProgressSteps/src/App.tsx
+++ ProgressSteps/src/App.tsx
@@ -4,18 +4,18 @@ import "./App.css";
 function App() {
   const [activeSteps, setActiveSteps] = useState(1);
   const [totalCircles, setTotalCircles] = useState(4);
-  console.log("activeSteps... ", activeSteps)
-  console.log("totalCircles... ", totalCircles)
+  console.log("activeSteps... ", activeSteps);
+  console.log("totalCircles... ", totalCircles);
 
   console.log(((activeSteps - 1) / (totalCircles - 1)) * 100 + "%");
 
   const nextHandler = () => {
-    setActiveSteps(activeSteps + 1)
-  }
+    setActiveSteps(activeSteps + 1);
+  };
 
   const prevHandler = () => {
-    setActiveSteps(activeSteps - 1)
-  }
+    setActiveSteps(activeSteps - 1);
+  };
   return (
     <div className="parent-container">
       <div className="child-container">
@@ -27,13 +27,25 @@ function App() {
           }}
         ></div>
         <div className="circle active">1</div>
-        <div className="circle active">2</div>
-        <div className="circle">3</div>
-        <div className="circle">4</div>
+        <div className={activeSteps >= 2 ? "circle active" : "circle"}>2</div> 
+        <div className={activeSteps >= 3 ? "circle active" : "circle"}>3</div>
+        <div className={activeSteps >= 4 ? "circle active" : "circle"}>4</div>
       </div>
       <div className="btn-container">
-        <button className="prev" onClick={prevHandler}>Prev</button>
-        <button className="next" onClick={nextHandler}>Next</button>
+        <button
+          className={activeSteps === 1 ? "prev disable" : "prev"}
+          onClick={prevHandler}
+          disabled={activeSteps === 1 ? true : false}
+        >
+          Prev
+        </button>
+        <button
+          className={activeSteps === totalCircles ? "next disable" : "next"}
+          onClick={nextHandler}
+          disabled={activeSteps === totalCircles ? true : false}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
