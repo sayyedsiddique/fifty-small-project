570bd73eef7517736cea60726eb43a6d45c992ca
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-12-23 22:30:36 +0530

    functionality done

--- HiddenSearchWidget/src/App.tsx
+++ HiddenSearchWidget/src/App.tsx
@@ -1,14 +1,26 @@
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
-import { IconContext } from 'react-icons';
+import { IconContext } from "react-icons";
+import { useState } from "react";
 
 function App() {
+  const [activeSearch, setActiveSearch] = useState(false);
+
+  const searchHandler = () => {
+    setActiveSearch(!activeSearch);
+  };
   return (
     <div className="parent">
-      <div className="child">
-        <input type="search" placeholder="Search..."/>
-        <div className="icon-container">
-          <IconContext.Provider value={{size: "30px" }}>
+      <div className={activeSearch ? "child active" : "child"}>
+        <div
+          className={
+            activeSearch ? "input-container active" : "input-container"
+          }
+        >
+          <input className="searchInput active" type="search" placeholder="Search..." />
+        </div>
+        <div className="icon-container" onClick={searchHandler}>
+          <IconContext.Provider value={{ size: "30px" }}>
             <FaSearch />
           </IconContext.Provider>
         </div>
