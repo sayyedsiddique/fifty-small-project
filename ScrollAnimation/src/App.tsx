7556df8d40a1b8c79ad8605ad90344ab76475973
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-12-25 22:36:07 +0530

    scroll animation design completed

---
+++ ScrollAnimation/src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="parent">
+      <h1>Scroll to see the animation</h1>
+      
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
