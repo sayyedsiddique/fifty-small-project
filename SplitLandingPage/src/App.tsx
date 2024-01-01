d397e41c41561a0c7da96ab747173f89bfc2848e
sayyedsiddique <sayyedsiddique055@gmail.com>
2024-01-01 22:25:42 +0530

    hovering effect functionality implemented

--- SplitLandingPage/src/App.tsx
+++ SplitLandingPage/src/App.tsx
@@ -2,26 +2,50 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [leftActive, setLeftActive] = useState(true)
-  console.log("leftActive ", leftActive)
+  const [leftActive, setLeftActive] = useState(false);
+  console.log("leftActive ", leftActive);
+  const [rightActive, setRightActive] = useState(false);
 
-  const leftHoverEnterHandler = (e) => {
-    console.log("e enter", e)
-    // setLeftActive(true)
-  }
+  const leftHoverEnterHandler = () => {
+    setLeftActive(true);
+  };
 
-  const leftHoverLeaveHandler = (e) => {
+  const leftHoverLeaveHandler = () => {
     // console.log("e leave", e)
-    // setLeftActive(false)
-  }
+    setLeftActive(false);
+  };
+
+  const rightHoverEnterHandler = () => {
+    setRightActive(true);
+  };
+
+  const rightHoverLeaveHandler = () => {
+    setRightActive(false);
+  };
 
   return (
-    <div className={leftActive ? "parent hover-left" : "parent"}>
-      <div className="split-container left" onMouseEnter={leftHoverEnterHandler} onMouseLeave={leftHoverLeaveHandler}>
+    <div
+      className={
+        leftActive
+          ? "parent hover-left"
+          : rightActive
+          ? "parent hover-right"
+          : "parent"
+      }
+    >
+      <div
+        className="split-container left"
+        onMouseEnter={leftHoverEnterHandler}
+        onMouseLeave={leftHoverLeaveHandler}
+      >
         <h1>Playstation 5</h1>
         <button>Buy Now</button>
       </div>
-      <div className="split-container right">
+      <div
+        className="split-container right"
+        onMouseEnter={rightHoverEnterHandler}
+        onMouseLeave={rightHoverLeaveHandler}
+      >
         <h1>XBox Series X</h1>
         <button>Buy Now</button>
       </div>
