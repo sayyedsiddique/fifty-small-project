9929f0a8844dd6b9689480c346e0be7ccabdab27
sayyedsiddique <sayyedsiddique055@gmail.com>
2024-01-02 22:28:30 +0530

    sound board buttons and sound add and some design implemented

---
+++ SoundBoard/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="parent">
+      <div className="child-container">
+        <button className="btn">applause</button>
+        <button className="btn">boo</button>
+        <button className="btn">gasp</button>
+        <button className="btn">tada</button>
+        <button className="btn">victory</button>
+        <button className="btn">wrong</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
