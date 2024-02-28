62c407e411fb7e00df0157a0e5d3eee80981068f
sayyedsiddique <sayyedsiddique055@gmail.com>
2024-02-28 21:54:09 +0530

    key event project design completed

--- EventKeyCodes/src/App.tsx
+++ EventKeyCodes/src/App.tsx
@@ -3,7 +3,26 @@ import "./App.css";
 function App() {
   return (
     <div className="parent">
-      <div className={"child"}></div>
+      <div className="child gap-10 flex justify-between align-items-center flex-wrap">
+        <div className="parent-box">
+          <p className="font-semibold">event.key</p>
+          <div className="box shadow-md bg-slate-100 text-xl  text-center font-bold p-4 rounded-sm">
+            <h5>Alt</h5>
+          </div>
+        </div>
+        <div className="parent-box">
+          <p className="semibold">event.keyCode</p>
+          <div className="box shadow-md bg-slate-100 text-xl  text-center font-bold p-4 rounded-sm">
+            <h5>18</h5>
+          </div>
+        </div>
+        <div className="parent-box">
+          <p className="semibold">event.code</p>
+          <div className="box shadow-md bg-slate-100 text-xl  text-center font-bold p-4 rounded-sm">
+            <h5>AltLeft</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
